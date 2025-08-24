a0f131026f036d23fb6c59ddbff75ac49272cb47
Alexey Anisimov <al.2005.anisimov@gmail.com>
2025-08-24 13:38:46 +0300

    l2.2

--- l1.2/l12.go
+++ l1.2/l12.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func sqaure(i int, nums []int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(nums[i] * nums[i])
+}
+
 func main() {
+	var wg sync.WaitGroup
+	nums := []int{2, 4, 6, 8, 10}
+	for i := range nums {
+		wg.Add(1)
+		go sqaure(i, nums, &wg)
+	}
+	wg.Wait()
 
 }
