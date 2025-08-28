4320f2aeb21b6a0f6d9872ac677148b643efd586
Alexey Anisimov <al.2005.anisimov@gmail.com>
2025-08-28 19:29:39 +0300

    l1.16

---
+++ l1.16/l116.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func quicksort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+
+	left := []int{}
+	equal := []int{}
+	right := []int{}
+
+	elem := nums[len(nums)/2]
+
+	for _, num := range nums {
+		if num < elem {
+			left = append(left, num)
+		} else if elem == num {
+			equal = append(equal, num)
+		} else {
+			right = append(right, num)
+		}
+	}
+	return append(append(quicksort(left), equal...), quicksort(right)...)
+}
+
+func main() {
+	nums := []int{3}
+	fmt.Println(quicksort(nums))
+}
